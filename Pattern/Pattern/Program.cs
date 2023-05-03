2e33dfd4a3f235e5b4353aec417dc82a9776849b
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-03 23:11:27 +0530

    Add files via upload

---
+++ Pattern/Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for(int i=1;i<n;i++)
+            {
+                for(int j=1;j<=i;j++)
+                
+                    Console.Write("*");
+                    Console.WriteLine();
+                
+            }
+        }
+    }
+}
---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt;
+            string result;
+            Console.WriteLine("Enter the string");
+            txt = Console.ReadLine();
+            //Program p=new Program();
+            result = Reverse(txt);
+            Console.WriteLine(result);
+
+        }
+        public static string Reverse(string txt) 
+        {
+            string rev=string.Empty;
+            char[] ca= txt.ToCharArray();
+            for(int i=ca.Length-1; i>=0; i--) 
+            {
+                rev += ca[i];
+            }
+            return rev;
+        }        
+    }
+}
